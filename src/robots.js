38fd2029a9efccf6b9512d28bf3a290e00146a56
kingston <64131655+kingston-uiix@users.noreply.github.com>
2020-07-26 13:29:54 +0530

    Update robots.js

--- src/robots.js
+++ src/robots.js
@@ -1,9 +1,9 @@
 export const robots = [
     {
       id: 1,
-      name: 'Leanne Graham',
-      username: 'Bret',
-      email: 'Sincere@april.biz'
+      name: 'kingston david',
+      username: 'king123',
+      email: 'kingstondavid1998@gmail.com'
     },
     {
       id: 2,
