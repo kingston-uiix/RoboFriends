9e61fcd8e3756ac652ceaee1fc83c6aee78ed8e7
Leander Leo Lagardo <leander.leo99@hotmail.com>
2020-07-26 12:35:36 +0530

    Updates

---
+++ precache-manifest.2d054396213a36315d84e12231a0acb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee41fb6bf9f6f1a6c1a27ae2865d1e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c985356d5daffdde44c1",
+    "url": "/static/css/main.91f6543e.chunk.css"
+  },
+  {
+    "revision": "cadb83d287e325f91aec",
+    "url": "/static/js/2.fa1425df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa1425df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c985356d5daffdde44c1",
+    "url": "/static/js/main.170f3360.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "9326738a9249692cf9088e58e53707e6",
+    "url": "/static/media/Roshi.9326738a.ttf"
+  },
+  {
+    "revision": "74e811e45f0086e6fed80fc2101518f7",
+    "url": "/static/media/SEREI___.74e811e4.TTF"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.6cace44422f892065646faa8f9688138.js"
+  "/precache-manifest.2d054396213a36315d84e12231a0acb0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
